61c4ea25dd6178379ed25d47ddaaf258a3edc3b2
han1202012 <octopus_truth@163.com>
2020-02-14 00:54:31 +0800

    update

--- app/src/main/java/kim/hsl/mk/MainActivity.java
+++ app/src/main/java/kim/hsl/mk/MainActivity.java
@@ -7,16 +7,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
 
         //6.0 以下的版本需要加载依赖库 , 6.0 以上的版本不需要加载依赖库
         //System.loadLibrary("add");
-
-        //System.loadLibrary("native-lib");
-
-        //System.loadLibrary("Test");
-        System.loadLibrary("hello-jni");
+        System.loadLibrary("native-lib");
     }
 
     @Override
@@ -24,18 +19,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        //TextView tv = findViewById(R.id.sample_text);
-        //tv.setText(stringFromJNI());
-
-        nativeTest();
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    //public native String stringFromJNI();
-
-    native void nativeTest();
+    public native String stringFromJNI();
 }
