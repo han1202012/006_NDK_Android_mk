7670d0ba071e69b035bb03d3da3dcbe6a1838100
han1202012 <octopus_truth@163.com>
2020-02-15 01:08:36 +0800

    update

--- app/src/main/java/kim/hsl/mk/MainActivity.java
+++ app/src/main/java/kim/hsl/mk/MainActivity.java
@@ -9,11 +9,14 @@ public class MainActivity extends AppCompatActivity {
 
     static {
 
-        // 加载动态库的情况 :
+        // 1 . 加载动态库的情况 : ( 必须不能注释下面的代码 )
         //      ① 6.0 以下的版本 : 需要手动加载依赖库 libadd.so
         //      ② 6.0 以上的版本 : 无法使用 Android.mk 构建脚本加载第三方动态库
         //                         此情况下, 无论是否手动加载 libadd.so 都会报错
-        System.loadLibrary("add");
+        //
+        // 2 . 加载静态库的情况 : ( 必须注释掉下面的这行代码 )
+        //System.loadLibrary("add");
+
         System.loadLibrary("native-lib");
     }
 
--- app/src/main/ndkBuild_Shared/native-lib.c
+++ app/src/main/ndkBuild_Shared/native-lib.c
@@ -14,18 +14,16 @@ Java_kim_hsl_mk_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject obj) {
 
-    __android_log_print(ANDROID_LOG_INFO, "JNI_TAG", "Hello from C");
-
     //调用动态库中的函数
     int sum = add(1, 2);
-    //打印计算结果
+
+    // Logcat 打印计算结果
     __android_log_print(ANDROID_LOG_INFO, "JNI_TAG", "Native Caculate : %d + %d = %d", 1, 2, sum);
 
     //将加法运算转为字符串
     char str[50] = "0";
-    sprintf(str, "Native Caculate : %d + %d = %d", 1, 2, sum);
-
-
+    //字符串格式化
+    sprintf(str, "Native Caculate : Shared Library : %d + %d = %d", 1, 2, sum);
 
     return (*env)->NewStringUTF(env, str);
 }
